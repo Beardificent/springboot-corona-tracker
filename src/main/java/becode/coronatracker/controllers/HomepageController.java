ea931592eb09222c0320d0696bc9c96b2b0e568e
Beardificent <cis.magito@gmail.com>
2020-12-23 15:41:31 +0100

    inital commit after killing teenager

---
+++ src/main/java/becode/coronatracker/controllers/HomepageController.java
@@ -0,0 +1,30 @@
+package becode.coronatracker.controllers;
+
+import becode.coronatracker.models.LocationStats;
+import becode.coronatracker.services.CoronaDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomepageController {
+
+    @Autowired
+    CoronaDataService coronaDataService;
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<LocationStats> allStats = coronaDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats",allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "index";
+    }
+}
+
